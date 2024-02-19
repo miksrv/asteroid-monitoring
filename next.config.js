4d109b492df96d8ffb77fa2db4c4dc86c932bbe7
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-02-19 15:04:03 -0800

    Revert "Upgraded UI libraries and CI/CD Deploy"

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: false
+    },
     // unoptimized - When true, the source image will be served as-is instead of changing quality,
     // size, or format. Defaults to false.
     images: {
