b9e646abfbfee5b43bcb1e0d942b1dc08d9a4187
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-04-05 21:31:53 -0700

    Refactoring API, added new endpoint

--- api/api.ts
+++ api/api.ts
@@ -1,20 +1,25 @@
-import { RootState } from '@/api/store'
-import { ApiNasaResponse } from '@/api/types'
-import type { Action, PayloadAction } from '@reduxjs/toolkit'
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { HYDRATE } from 'next-redux-wrapper'
 
-const isHydrateAction = (action: Action): action is PayloadAction<RootState> =>
-    action.type === HYDRATE
+import { RootState } from '@/api/store'
+import { ApiNasaResponse, Asteroid } from '@/api/types'
+import type { Action, PayloadAction } from '@reduxjs/toolkit'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const isHydrateAction = (action: Action): action is PayloadAction<RootState> => action.type === HYDRATE
+// http://api.nasa.gov/neo/rest/v1/neo/2474163?api_key=lCr0fgpblRx6TI1nzHRvFDh2e8fWrw2ARXMe4H7Q
+// https://api.nasa.gov/neo/rest/v1/feed
+const API_URL = 'https://api.nasa.gov/neo/rest/v1/'
 
 export const API = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.NEXT_PUBLIC_API_URL
+        baseUrl: API_URL
     }),
     endpoints: (builder) => ({
-        getAsteroids: builder.mutation<ApiNasaResponse, string>({
-            query: (date) =>
-                `?api_key=${process.env.NEXT_PUBLIC_API_KEY}&start_date=${date}&end_date=${date}`
+        getAsteroidsList: builder.mutation<ApiNasaResponse, string>({
+            query: (date) => `feed?api_key=${process.env.NEXT_PUBLIC_API_KEY}&start_date=${date}&end_date=${date}`
+        }),
+        getAsteroidData: builder.mutation<Asteroid, number>({
+            query: (id) => `neo/${id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
         })
     }),
     extractRehydrationInfo(action, { reducerPath }): any {
--- api/store.ts
+++ api/store.ts
@@ -1,7 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
 import { createWrapper } from 'next-redux-wrapper'
 
 import { API } from './api'
+
+import { configureStore } from '@reduxjs/toolkit'
 
 export const store = () =>
     configureStore({
@@ -13,6 +14,5 @@ export const store = () =>
 
 export type AppStore = ReturnType<typeof store>
 export type RootState = ReturnType<AppStore['getState']>
-// export type AppDispatch = AppStore['dispatch']
 
 export const wrapper = createWrapper<AppStore>(store, { debug: false })
