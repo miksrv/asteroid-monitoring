5c070076bf39fa930764c2ebb0b0852a9e71ccd5
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-04-27 10:25:34 -0700

    Implemented new UI Detailed component for show asteroid orbit

---
+++ components/Detailed/Detailed.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { Skeleton } from 'simple-react-ui-kit'
+
+import API from '@/api/api'
+import Spacemap from '@/components/Spacemap'
+
+type DetailedProps = {
+    asteroidId?: number
+    clientHeight?: number
+}
+
+export const Detailed: React.FC<DetailedProps> = ({ asteroidId, clientHeight = 200 }) => {
+    const [getAsteroidData, { data: asteroidData, isLoading: asteroidLoading }] = API.useGetAsteroidDataMutation()
+
+    useEffect(() => {
+        if (asteroidId) {
+            getAsteroidData(asteroidId)
+        }
+    }, [asteroidId])
+
+    return (
+        <div style={{ height: `${clientHeight - 200}px` }}>
+            {asteroidLoading || !asteroidData?.orbital_data ? (
+                <Skeleton style={{ width: '100%', height: '100%' }} />
+            ) : (
+                <Spacemap
+                    asteroidName={asteroidData?.name}
+                    orbitalData={asteroidData?.orbital_data}
+                />
+            )}
+        </div>
+    )
+}
+
+export default Detailed
